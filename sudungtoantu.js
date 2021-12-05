e7daa219899ebbaffbe2240abbec1206a6bd5865
Nguyễn Hiếu <batboy_boy@yahoo.com>
2021-12-06 06:23:05 +0700

    done
    done

---
+++ sudungtoantu.js
@@ -0,0 +1,7 @@
+
+let inputWidth = prompt("nhap do rong");
+let inputHeight = prompt("nhap chieu cao"); 
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let ketqua = width * height;
+document.write("S hinh chu nhat=" + ketqua);
